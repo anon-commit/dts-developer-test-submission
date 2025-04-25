64f1c50806453d9f73fc5dc4504967f27f2f3f16
anon-commit <committedbyanon@gmail.com>
2025-04-25 14:06:44 +0100

    fix: remove broken import

--- src/main/models/Task.ts
+++ src/main/models/Task.ts
@@ -24,7 +24,6 @@ import type {
   IInsertTaskParams,
   IGetTasksByDueDateDescParams,
 } from "../queries/taskQueries.queries.js";
-import type { SortOrder } from "../util/types.js";
 import { TaskNotFoundError } from "../util/errors.js";
 
 /*
