e1d73704f24e877a7abadbe47acb55847eef17ff
anon-commit <committedbyanon@gmail.com>
2025-04-25 17:24:02 +0100

    chore: add test db

--- src/main/db/index.ts
+++ src/main/db/index.ts
@@ -3,12 +3,23 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: [".env.local", ".env"] });
 
-const pool = new pg.Pool({
-  database: "task_manager",
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT!),
-});
+let pool: pg.Pool;
+if (process.env.DB_PROD === "true") {
+  pool = new pg.Pool({
+    database: "task_manager",
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT!),
+  });
+} else {
+  pool = new pg.Pool({
+    database: "task_manager_test",
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT!),
+  });
+}
 
 export { pool };
